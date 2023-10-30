960c214ffc91560427dc9de324fd5827abcbd003
jinseoplee <jjjp9966@gmail.com>
2023-10-31 00:51:38 +0900

    add 1149

---
+++ Dynamic Programming/1149/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 입력
+		int n = Integer.parseInt(br.readLine());
+
+		int[][] dp = new int[n + 1][3];
+		for (int i = 1; i <= n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			dp[i][0] += Math.min(dp[i - 1][1], dp[i - 1][2]) + Integer.parseInt(st.nextToken());
+			dp[i][1] += Math.min(dp[i - 1][0], dp[i - 1][2]) + Integer.parseInt(st.nextToken());
+			dp[i][2] += Math.min(dp[i - 1][0], dp[i - 1][1]) + Integer.parseInt(st.nextToken());
+		}
+
+		// 출력
+		System.out.println(Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2])));
+	}
+}
