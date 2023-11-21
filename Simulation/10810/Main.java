e6a27a155ee95d03d85d1c20ff568e5113b5dc89
jinseoplee <jjjp9966@gmail.com>
2023-11-21 22:41:06 +0900

    add 10810

---
+++ Simulation/10810/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[n + 1];
+		while (m-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+
+			for (int l = i; l <= j; l++) {
+				arr[l] = k;
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+			sb.append(arr[i]).append(" ");
+		}
+		System.out.println(sb);
+	}
+}
