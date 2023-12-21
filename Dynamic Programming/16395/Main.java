a776095f18f37618575638162479bb805b455e43
jinseoplee <jjjp9966@gmail.com>
2023-12-22 07:49:36 +0900

    add 16395

---
+++ Dynamic Programming/16395/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int n = Integer.parseInt(input[0]);
+		int k = Integer.parseInt(input[1]);
+		int[][] dp = new int[n + 1][n + 1];
+		dp[1][1] = 1;
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+			}
+		}
+		System.out.println(dp[n][k]);
+	}
+}
