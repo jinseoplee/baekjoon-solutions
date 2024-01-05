42932cbd003a9615689326f5ac81e70ac205a374
jinseoplee <jjjp9966@gmail.com>
2024-01-05 20:32:23 +0900

    add 1965

---
+++ Dynamic Programming/1965/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int maxBoxCnt = 0;
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[i] = 1;
+
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			maxBoxCnt = Math.max(maxBoxCnt, dp[i]);
+		}
+
+		System.out.println(maxBoxCnt);
+	}
+}
