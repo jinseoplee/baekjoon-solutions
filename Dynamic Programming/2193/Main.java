874bda283179771befddc7fc0497ff80305b6c2c
jinseoplee <jjjp9966@gmail.com>
2024-01-19 17:18:29 +0900

    add 2193

---
+++ Dynamic Programming/2193/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		// dp 테이블 생성 및 초기화
+		long[] dp = new long[n + 1];
+		dp[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		System.out.println(dp[n]);
+	}
+}
